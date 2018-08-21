e05114af3de997237ba2be168cc86d7ef73c4246
Sam Bengtson <sam.bengtson@gmail.com>
2018-08-21 12:00:55 -0500

    full circle payment

--- index.js
+++ index.js
@@ -12,75 +12,70 @@ function spend() {
 
   const wif = 'cNgciJr2mLDL9hxX7z7TQRuV5TR5C8irJy6tJRyZZhwhJkt1PEmr';
   const ecPair = BITBOX.ECPair.fromWIF(wif);
-  const address = BITBOX.ECPair.toLegacyAddress(ecPair);
+  const address = BITBOX.ECPair.toCashAddress(ecPair);
 
   console.log(address);
-  // if (!BITBOX.Address.isTestnetAddress(address)) {
-  //   throw 'This is not a testnet address!!'
-  // }
+  if (!BITBOX.Address.isTestnetAddress(address)) {
+    throw 'This is not a testnet address!!'
+  }
 
-  BITBOX.Address.details(address).then(result => {
-    console.log(result);
-  }, err => {
-    console.log(err)
-  })
+  let balance = 0;
+  BITBOX.Address.details(address)
+    .then(result => {
+      balance = result.balance;
+      console.log(result);
+      return BITBOX.Address.utxo(address)
+    })
+    .then(result => {
+      if (!result[0]) {
+        return;
+      }
 
-  BITBOX.Address.utxo(address).then((result) => {
-    console.log('result time!');
-    console.log(result);
-    if (!result[0]) {
-      return;
-    }
+      // instance of transaction builder
+      let transactionBuilder = new BITBOX.TransactionBuilder('testnet');
+      // original amount of satoshis in vin
+      let originalAmount = result[0].satoshis;
 
-    // instance of transaction builder
-    let transactionBuilder = new BITBOX.TransactionBuilder('bitcoincash');
-    // original amount of satoshis in vin
-    let originalAmount = result[0].satoshis;
+      // index of vout
+      let vout = result[0].vout;
 
-    // index of vout
-    let vout = result[0].vout;
+      // txid of vout
+      let txid = result[0].txid;
 
-    // txid of vout
-    let txid = result[0].txid;
+      // add input with txid and index of vout
+      transactionBuilder.addInput(txid, vout);
 
-    // add input with txid and index of vout
-    transactionBuilder.addInput(txid, vout);
+      // get byte count to calculate fee. paying 1 sat/byte
+      let byteCount = BITBOX.BitcoinCash.getByteCount({
+        P2PKH: 1
+      }, {
+          P2PKH: 1
+        });
+      // 192
+      // amount to send to receiver. It's the original amount - 1 sat/byte for tx size
+      let sendAmount = originalAmount - byteCount;
 
-    // get byte count to calculate fee. paying 1 sat/byte
-    let byteCount = BITBOX.BitcoinCash.getByteCount({
-      P2PKH: 1
-    }, {
-      P2PKH: 1
-    });
-    // 192
-    // amount to send to receiver. It's the original amount - 1 sat/byte for tx size
-    let sendAmount = originalAmount - byteCount;
+      // add output w/ address and amount to send
+      transactionBuilder.addOutput(address, sendAmount);
 
-    // add output w/ address and amount to send
-    transactionBuilder.addOutput(cashAddress, sendAmount);
+      // sign w/ HDNode
+      let redeemScript;
+      transactionBuilder.sign(0, ecPair, redeemScript, transactionBuilder.hashTypes.SIGHASH_ALL, originalAmount);
 
-    // keypair
-    let keyPair = BITBOX.HDNode.toKeyPair(change);
+      // build tx
+      let tx = transactionBuilder.build();
+      // output rawhex
+      let hex = tx.toHex();
+      console.log(`Transaction raw hex: ${hex}`);
 
-    // sign w/ HDNode
-    let redeemScript;
-    transactionBuilder.sign(0, keyPair, redeemScript, transactionBuilder.hashTypes.SIGHASH_ALL, originalAmount);
-
-    // build tx
-    let tx = transactionBuilder.build();
-    // output rawhex
-    let hex = tx.toHex();
-    console.log(`Transaction raw hex: ${hex}`);
-
-    // sendRawTransaction to running BCH node
-    // BITBOX.RawTransactions.sendRawTransaction(hex).then((result) => {
-    //   console.log(`Transaction ID: ${result}`);
-    // }, (err) => {
-    //   console.log(err);
-    // });
-  }, (err) => {
-    console.log(err);
-  });
+      return BITBOX.RawTransactions.sendRawTransaction(hex)
+    })
+    .then(tx => {
+      console.log(tx);
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 function createAccount() {
